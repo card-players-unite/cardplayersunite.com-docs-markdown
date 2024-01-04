64121d190000f93b01bc80a79e09e9182a85505c
Software Shinobi <the.software.shinobi@gmail.com>
2024-01-04 15:07:59 -0500

    automated terminal push

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
-
-reset
-
-clear
-
-##
-
-pwd
-
-cd project-documentation
-
-pwd
+#!/bin/bash
 
 ##
 
@@ -23,6 +12,4 @@ docker-compose -f .docker-compose.yaml down --remove-orphans
 
 docker-compose -f .docker-compose.yaml up
 
-## firefox http://localhost:8888 &
-
-## docker run --rm -it -p $localPort:8000 -v ${PWD}:/docs titom73/mkdocs
+## firefox http://localhost:8000 &
